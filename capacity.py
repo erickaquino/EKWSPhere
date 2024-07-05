7da7690d9e582065dc6c37f4052183cc11101ade
Erick Joshua Aquino <ejaquino26@gmail.com>
2024-07-06 07:18:41 +0800

    Working

--- capacity.py
+++ capacity.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from mangum import Mangum
+import requests
+import json
+import asyncio
+import aiohttp
 
 app = FastAPI()
 handler = Mangum(app)
@@ -9,6 +13,15 @@ handler = Mangum(app)
 class DataModel(BaseModel):
     data: str
 
+class CapacityAuthentication(BaseModel):
+    client_secret: str
+    client_id: str
+
+class CapacityShipmentConfirmation(BaseModel):
+    token: str
+    key: str
+    batch_date: str
+    max_batches: int
 
 @app.get("/")
 def index():
@@ -19,12 +32,65 @@ def index():
 def processs_post(data: DataModel):
     return data
 
+@app.post("/getToken")
+def get_token(data: CapacityAuthentication):
+    client_id = data.client_id
+    client_secret = data.client_secret
+    url ="https://api-integration.capacityllc.com/token"
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    myjson = {
+        'client_secret': client_secret,
+        'client_id': client_id,
+        'grant_type': 'client_credentials'
+    }
+    body = requests.post(url,data=myjson).json()
+    if "access_token" in body:
+        return {
+            'access_token': body['access_token']
+        }
+    else:
+        return {
+            'access_token': ''
+        }
 
-@app.post("/")
-def processs_post(data: DataModel):
-    return data
+@app.post("/getShipmentConfirmation")
+async def get_shipment_confirmations(data: CapacityShipmentConfirmation):
+    semaphore = asyncio.Semaphore(data.max_batches)
+    tasks = [sub_get_confirmation_data(i, data, semaphore) for i in range(1, data.max_batches + 1)]
+    results = await asyncio.gather(*tasks)
+    finalArr = []
+    for result in results:
+        finalArr.extend(result['orders'])
+    return {
+        'batch_date': data.batch_date,
+        'orders': finalArr
+    }
 
-
-@app.post("/test/{data}")
-def process_post(data):
-    return data
+async def sub_get_confirmation_data(batch_number, data: CapacityShipmentConfirmation, semaphore):
+    async with semaphore:
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {data.token}"
+        }
+        payload = json.dumps({
+            "key": data.key,
+            "batch_num": batch_number,
+            "batch_date": data.batch_date
+        })
+        url = "https://api-integration.capacityllc.com/api/order/track"
+        #print(payload)
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, headers=headers, data=payload, ssl=False) as response:
+                body = await response.json()
+                if "orderlist" in body:
+                    return {
+                        "batch_number": batch_number,
+                        "orders": [order['masterorderid'] for order in body['orderlist']]
+                    }
+                else:
+                    return {
+                        "batch_number": batch_number,
+                        "orders": []
+                    }
--- send_requests.py
+++ send_requests.py
@@ -1,10 +1,26 @@
 import requests
 
-url ="http://127.0.0.1:8000"
-myjson = {'data': 'somevalue'}
+url ="http://127.0.0.1:8000/getToken"
+ship_confirm_url = "http://127.0.0.1:8000/getShipmentConfirmation"
+myjson = {
+    'client_secret': 'fqju**J@U)N^d}`q8tQ',
+    'client_id': 'juststeven.capacity.client',
+    'grant_type': 'client_credentials'
+}
 
+shipConfirmPayload = {
+    "key": "CD343BFA-3109-44D6-A4CE-63313F98B8C1",
+    "batch_date": "2024-07-01",
+    "token": None,
+    "max_batches": 2
+}
 
+token = requests.post(url,json=myjson).json()['access_token']
+print(token)
 
-print(requests.get(url).json())
+if token != "":
+    shipConfirmPayload['token'] = token
+    print(shipConfirmPayload)
+    ship_confirm_data = requests.post(ship_confirm_url,json=shipConfirmPayload).json()
+    print(ship_confirm_data)
 
-print(requests.post(url,json=myjson).json())
