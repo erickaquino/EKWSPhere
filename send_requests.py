f37c0bb93aee47b8461d338fc6cb7a0cc20d9f9f
Erick Joshua Aquino <ejaquino26@gmail.com>
2024-07-29 08:57:24 +0800

    Working

--- send_requests.py
+++ send_requests.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 celigoUrlTest = 'http://127.0.0.1:8000/testCeligoConnection'
 celigoUrlExport = 'http://127.0.0.1:8000/runCeligoExport'
@@ -6,31 +7,32 @@ celigoUrlFlow = 'http://127.0.0.1:8000/runCeligoFlow'
 url ="http://127.0.0.1:8000/getToken"
 ship_confirm_url = "http://127.0.0.1:8000/getOrders"
 ship_confirm_url_specific = "http://127.0.0.1:8000/getShipmentConfirmation/specific"
-# JONES_ROAD myjson = {
-#     'client_secret': 'fqju**J@U)N^d}`q8tQ',
-#     'client_id': 'juststeven.capacity.client'
-# }
-
-# JONES_ROAD shipConfirmPayload = {
-#     "key": "CD343BFA-3109-44D6-A4CE-63313F98B8C1",
-#     "start_date": "2024-07-01",
-#     "end_date": "2024-07-01",
-#     "token": None,
-#     "max_batches": 24
-# }
 
 myjson = {
-    'client_secret': '*u6d8ev$0B{qL8B',
-    'client_id': 'lysecomm.capacity.client'
+    'client_secret': 'fqju**J@U)N^d}`q8tQ',
+    'client_id': 'juststeven.capacity.client'
 }
 
 shipConfirmPayload = {
-    "key": "EDAE07E5-9710-4224-BD37-F95CF39A7151",
+    "key": "CD343BFA-3109-44D6-A4CE-63313F98B8C1",
     "start_date": "2024-07-01",
-    "end_date": "2024-07-01",
+    "end_date": "2024-07-30",
     "token": None,
-    "max_batches": 6
+    "max_batches": 24
 }
+
+# myjson = {
+#     'client_secret': '*u6d8ev$0B{qL8B',
+#     'client_id': 'lysecomm.capacity.client'
+# }
+
+# shipConfirmPayload = {
+#     "key": "EDAE07E5-9710-4224-BD37-F95CF39A7151",
+#     "start_date": "2024-07-01",
+#     "end_date": "2024-07-01",
+#     "token": None,
+#     "max_batches": 6
+# }
 
 
 specificOrderPayload = {
@@ -60,6 +62,8 @@ celigoFlowPayload = {
     'flow_id': '66819994e4787783082087d7'
 }
 
+
+start_time = time.time()
 token = requests.post(url,json=myjson).json()['access_token']
 print(token)
 
@@ -77,3 +81,6 @@ if token != "":
 #print(requests.post(celigoUrlExport,json=celigoExportPayload).json())
 #print(requests.post(celigoUrlExport,json=celigoExportPayload).json())
 
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"Time taken for execution: {execution_time:.2f} seconds")
